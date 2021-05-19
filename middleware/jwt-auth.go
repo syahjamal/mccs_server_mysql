7d5005a0c647de1a33accf5249063b00e945b353
syahjamal <sulaimansyahjamal@gmail.com>
2021-05-19 23:09:46 +0700

    jwt-auth middleware

---
+++ middleware/jwt-auth.go
@@ -0,0 +1,32 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"github.com/syahjamal/mccs_server_mysql/helper"
+	"github.com/syahjamal/mccs_server_mysql/service"
+)
+
+func AuthorizeJWT(jwtService service.JWTService) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authHeader := ctx.GetHeader("Authorization")
+		if authHeader == "" {
+			response := helper.BuildErrorResponse("Failed to process request", "No token found", nil)
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, response)
+			return
+		}
+		token, err := jwtService.ValidateToken(authHeader)
+		if token.Valid {
+			claims := token.Claims.(jwt.MapClaims)
+			log.Println("Claim[user_id]: ", claims["user_id"])
+			log.Println("Claim[issuer] :", claims["issuer"])
+		} else {
+			log.Println(err)
+			response := helper.BuildErrorResponse("Token is not valid", err.Error(), nil)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
+		}
+	}
+}
